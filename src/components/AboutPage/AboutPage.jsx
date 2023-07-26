f0cc7a12adf41728e22566a2abadfa7cf9e042dc
Steven Zhou <stevenz9@uci.edu>
2023-07-25 19:43:37 -0700

    stats shown

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,36 +1,94 @@
-import React from "react";
-import { Button, Text, Flex } from "@chakra-ui/react";
+import { React, useState } from "react";
+import { Button, Text, Flex, Stack, Icon } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
 
 export default function AboutPage() {
   const navigate = useNavigate();
+  const [screenWidth, setScreenWidth] = useState(window.screen.width);
+  window.onresize = () => {
+    setScreenWidth(window.screen.width);
+  };
+
   const handleClick = () => {
     navigate("/");
   };
 
   return (
-    <Flex justifyContent={"center"} alignItems={"center"} direction={"column"}>
-      <Text fontSize={{ base: "2rem", md: "3rem" }} as="b" width={"10em"}>
-        We are ZotnFound
-      </Text>
-      <Text fontSize={{ base: "1rem", md: "1.5rem" }} width={"40em"}>
+    <Flex
+      alignItems={"center"}
+      direction={"column"}
+      height={"100vh"}
+      paddingTop={"1em"}
+      gap={"1em"}
+      width={"100vw"}
+    >
+      <Stack
+        direction={{ base: "column", md: "column", lg: "row" }}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        width={"100vw"}
+      >
+        <Flex display={screenWidth < 992 ? "none" : "block"} marginLeft={10}>
+          <Button onClick={handleClick} visibility={"hidden"}>
+            <Icon as={ArrowBackIcon} marginRight={"2%"} />
+            Return Home{" "}
+          </Button>
+        </Flex>
+        <Text fontSize={{ base: "2rem", md: "3rem" }} as="b" width={"10em"}>
+          We are ZotnFound
+        </Text>
+        <Flex>
+          <Button onClick={handleClick} marginRight={{ lg: 10 }}>
+            <Icon as={ArrowBackIcon} marginRight={"2%"} />
+            Return Home{" "}
+          </Button>
+        </Flex>
+      </Stack>
+      <Text
+        fontSize={{ base: "1rem", md: "1.5rem" }}
+        width={{ base: "20em", md: "40em" }}
+      >
         insert bio Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
         quae distinctio corporis officiis neque incidunt placeat, doloribus
         minus atque qui in accusamus architecto quidem excepturi maxime
         reiciendis, harum est sequi?
       </Text>
       <Flex
-        justifyContent={"space-between"}
         direction={"row"}
+        justifyContent="center"
         alignItems={"center"}
+        gap={"1em"}
+        background={"#FBF7F6"}
+        width={"100vw"}
       >
-        <Text>Lost Items</Text>
-        <Text>Found Items</Text>
-        <Text>Successful Returns</Text>
-        <Text>Active Users</Text>
-        <Text>test</Text>
+        <Flex direction={"column"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+            0
+          </Text>
+          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>Lost Items</Text>
+        </Flex>
+        <Flex direction={"column"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+            0
+          </Text>
+          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>Found Items</Text>
+        </Flex>
+        <Flex direction={"column"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+            0
+          </Text>
+          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>
+            Successful Returns
+          </Text>
+        </Flex>
+        <Flex direction={"column"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+            0
+          </Text>
+          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>Active Users</Text>
+        </Flex>
       </Flex>
-      <Button onClick={handleClick}>Back </Button>
     </Flex>
   );
 }
