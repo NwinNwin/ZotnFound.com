47b45bb1d6ffdf21e0807d64befe349999c22755
Steven Zhou <stevenz9@uci.edu>
2023-07-28 02:52:04 -0700

    carousel finished

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -70,32 +70,35 @@ export default function AboutPage() {
         alignItems={"center"}
         gap={"1em"}
         width={"100vw"}
+        bg={"#fffcf6"}
+        mt={"1%"}
+        mb={"1%"}
       >
-        <Flex direction={"column"}>
-          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+        <Flex direction={"column"} m={"1%"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "2.4rem" }}>
             0
           </Text>
-          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>Lost Items</Text>
+          <Text fontSize={{ base: "0.8rem", md: "1.2rem" }}>Lost Items</Text>
         </Flex>
-        <Flex direction={"column"}>
-          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+        <Flex direction={"column"} m={"1%"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "2.4rem" }}>
             0
           </Text>
-          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>Found Items</Text>
+          <Text fontSize={{ base: "0.8rem", md: "1.2rem" }}>Found Items</Text>
         </Flex>
-        <Flex direction={"column"}>
-          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+        <Flex direction={"column"} m={"1%"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "2.4rem" }}>
             0
           </Text>
-          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>
+          <Text fontSize={{ base: "0.8rem", md: "1.2rem" }}>
             Successful Returns
           </Text>
         </Flex>
-        <Flex direction={"column"}>
-          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+        <Flex direction={"column"} m={"1%"}>
+          <Text fontWeight={600} fontSize={{ base: "1.3rem", md: "2.4rem" }}>
             0
           </Text>
-          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>Active Users</Text>
+          <Text fontSize={{ base: "0.8rem", md: "1.2rem" }}>Active Users</Text>
         </Flex>
       </Flex>
       <Flex
@@ -163,162 +166,175 @@ export default function AboutPage() {
           <Image src={logo} w="15em" h="15em" />
         </Flex>
       </Flex>
-        <Flex
-          flexBasis={"100%"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          direction={"column"}
-          bg={"#fffcf6"}
-          w="100vw"
+      <Flex
+        flexBasis={"100%"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        direction={"column"}
+        bg={"#fffcf6"}
+        w="100vw"
+      >
+        <Text
+          fontWeight={500}
+          fontSize={{ base: "1.3rem", md: "1.6rem" }}
+          mb={"1%"}
+          mt={"1%"}
         >
-          <Text
-            fontWeight={500}
-            fontSize={{ base: "1.3rem", md: "1.6rem" }}
-            mb={"1%"}
-            mt={"1%"}
-            >
-            Explore how ZotnFound works
-          </Text>
-          <Flex width={"60em"} height={"40em"}>
-            <Swiper
-              spaceBetween={10}
-              slidesPerView={1}
-              modules={[Navigation, Pagination]}
-              navigation
-              pagination={{ clickable: true, dynamicBullets: true }}
-            >
-              <SwiperSlide>
-                <Flex
-                  direction="column"
-                  justifyContent={"center"}
-                  alignItems={"center"}
+          Explore how ZotnFound works
+        </Text>
+        <Flex width={"60em"} height={"40em"}>
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+          >
+            <SwiperSlide>
+              <Flex
+                direction="column"
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Image src={login_page} />
+                <Text
+                  fontWeight={500}
+                  fontSize={{ base: "1.3rem", md: "1.6rem" }}
+                  mt={"2%"}
                 >
-                  <Image src={login_page} />
-                  <Text
-                    fontWeight={500}
-                    fontSize={{ base: "1.3rem", md: "1.6rem" }}
-                    mt={"2%"}
-                  >
-                    Get Started - It's Simple & Easy
-                  </Text>
-                  <Text fontSize={{ base: "0.8rem", md: "1rem" }} mt={"2%"}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-                    ab deserunt nostrum, numquam veritatis quis odio voluptatum
-                    cupiditate harum eligendi dignissimos? Sint sit optio cumque
-                    delectus porro doloremque voluptas in?
-                  </Text>
-                </Flex>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Flex
-                  direction="column"
-                  justifyContent={"center"}
-                  alignItems={"center"}
+                  Get Started - It's Simple & Easy
+                </Text>
+                <Text
+                  fontSize={{ base: "0.8rem", md: "1rem" }}
+                  mt={"2%"}
+                  maxW={"40em"}
                 >
-                  <Image src={login_page} />
-                  <Text
-                    fontWeight={500}
-                    fontSize={{ base: "1.3rem", md: "1.6rem" }}
-                    mt={"2%"}
-                  >
-                    Navigate Around the Map
-                  </Text>
-                  <Text fontSize={{ base: "0.8rem", md: "1rem" }} mt={"2%"}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-                    ab deserunt nostrum, numquam veritatis quis odio voluptatum
-                    cupiditate harum eligendi dignissimos? Sint sit optio cumque
-                    delectus porro doloremque voluptas in?
-                  </Text>
-                </Flex>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Flex
-                  direction="column"
-                  justifyContent={"center"}
-                  alignItems={"center"}
+                  Log in with your UCI email and start listing lost & found
+                  items! {"(No need to sign up or create an account)."}
+                </Text>
+              </Flex>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Flex
+                direction="column"
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Image src={login_page} />
+                <Text
+                  fontWeight={500}
+                  fontSize={{ base: "1.3rem", md: "1.6rem" }}
+                  mt={"2%"}
                 >
-                  <Image src={login_page} />
-                  <Text
-                    fontWeight={500}
-                    fontSize={{ base: "1.3rem", md: "1.6rem" }}
-                    mt={"2%"}
-                  >
-                    Search for Lost & Found Items
-                  </Text>
-                  <Text fontSize={{ base: "0.8rem", md: "1rem" }} mt={"2%"}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-                    ab deserunt nostrum, numquam veritatis quis odio voluptatum
-                    cupiditate harum eligendi dignissimos? Sint sit optio cumque
-                    delectus porro doloremque voluptas in?
-                  </Text>
-                </Flex>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Flex
-                  direction="column"
-                  justifyContent={"center"}
-                  alignItems={"center"}
+                  Navigate Around the Map
+                </Text>
+                <Text
+                  fontSize={{ base: "0.8rem", md: "1rem" }}
+                  mt={"2%"}
+                  maxW={"40em"}
                 >
-                  <Image src={login_page} />
-                  <Text
-                    fontWeight={500}
-                    fontSize={{ base: "1.3rem", md: "1.6rem" }}
-                    mt={"2%"}
-                  >
-                    Upload Your Items - Help the Community!
-                  </Text>
-                  <Text fontSize={{ base: "0.8rem", md: "1rem" }} mt={"2%"}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-                    ab deserunt nostrum, numquam veritatis quis odio voluptatum
-                    cupiditate harum eligendi dignissimos? Sint sit optio cumque
-                    delectus porro doloremque voluptas in?
-                  </Text>
-                </Flex>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Flex
-                  direction="column"
-                  justifyContent={"center"}
-                  alignItems={"center"}
+                  Effortlessly navigate UCI's interactive map to efficiently
+                  search for lost and found items. Explore the digital landscape
+                  with ease as you locate and reclaim belongings!
+                </Text>
+              </Flex>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Flex
+                direction="column"
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Image src={login_page} />
+                <Text
+                  fontWeight={500}
+                  fontSize={{ base: "1.3rem", md: "1.6rem" }}
+                  mt={"2%"}
                 >
-                  <Image src={login_page} />
-                  <Text
-                    fontWeight={500}
-                    fontSize={{ base: "1.3rem", md: "1.6rem" }}
-                    mt={"2%"}
-                  >
-                    Contact Others and Connect
-                  </Text>
-                  <Text fontSize={{ base: "0.8rem", md: "1rem" }} mt={"2%"}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-                    ab deserunt nostrum, numquam veritatis quis odio voluptatum
-                    cupiditate harum eligendi dignissimos? Sint sit optio cumque
-                    delectus porro doloremque voluptas in?
-                  </Text>
-                </Flex>
-              </SwiperSlide>
-            </Swiper>
-          </Flex>
+                  Search for Lost & Found Items
+                </Text>
+                <Text
+                  fontSize={{ base: "0.8rem", md: "1rem" }}
+                  mt={"2%"}
+                  maxW={"40em"}
+                >
+                  Utilize the easy to use filter and search bar to look up
+                  specific items based off their name, description, type, or the
+                  time estimate of when the item was lost/found.
+                </Text>
+              </Flex>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Flex
+                direction="column"
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Image src={login_page} />
+                <Text
+                  fontWeight={500}
+                  fontSize={{ base: "1.3rem", md: "1.6rem" }}
+                  mt={"2%"}
+                >
+                  Upload Your Items - Join the Community!
+                </Text>
+                <Text
+                  fontSize={{ base: "0.8rem", md: "1rem" }}
+                  mt={"2%"}
+                  maxW={"40em"}
+                >
+                  Show off the different items that you may have found or lost.
+                  Participate in helping return items back to the community and
+                  stay on a lookout for any items that may be yours!
+                </Text>
+              </Flex>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Flex
+                direction="column"
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Image src={login_page} />
+                <Text
+                  fontWeight={500}
+                  fontSize={{ base: "1.3rem", md: "1.6rem" }}
+                  mt={"2%"}
+                >
+                  Contact Others and Connect
+                </Text>
+                <Text
+                  fontSize={{ base: "0.8rem", md: "1rem" }}
+                  mt={"2%"}
+                  maxW={"40em"}
+                >
+                  Meet people in the community and create new friends! Easily
+                  contact people who may have found your items and vice versa.
+                </Text>
+              </Flex>
+            </SwiperSlide>
+          </Swiper>
         </Flex>
-        <Flex
-          flexBasis={"100%"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          direction={"column"}
-        >
-          <Text fontWeight={500} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
-            Community Guidelines
-          </Text>
-          <Text fontSize={{ base: "0.8rem", md: "1rem" }}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
-            quibusdam sit nihil consectetur optio. Delectus ullam distinctio
-            illo, odio qui excepturi minima ab officiis dolores libero aliquam
-            eum natus provident. Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Consectetur aliquid explicabo ad doloremque maxime
-            est molestiae alias veritatis animi magni pariatur, vel architecto
-            facilis laboriosam facere iusto eum ea quo.
-          </Text>
-        </Flex>
+      </Flex>
+      <Flex
+        flexBasis={"100%"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        direction={"column"}
+      >
+        <Text fontWeight={500} fontSize={{ base: "1.3rem", md: "1.6rem" }}>
+          Community Guidelines
+        </Text>
+        <Text fontSize={{ base: "0.8rem", md: "1rem" }} maxW={"80em"}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
+          quibusdam sit nihil consectetur optio. Delectus ullam distinctio illo,
+          odio qui excepturi minima ab officiis dolores libero aliquam eum natus
+          provident. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Consectetur aliquid explicabo ad doloremque maxime est molestiae alias
+          veritatis animi magni pariatur, vel architecto facilis laboriosam
+          facere iusto eum ea quo.
+        </Text>
+      </Flex>
       <Stack height={"50"}>
         <Text>Footer</Text>
       </Stack>
