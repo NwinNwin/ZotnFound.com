68ec20f505ccbe45c28ef018f3d290fddacbcade
Dang Nguyen Nguyen <ndnguyen2003@gmail.com>
2023-09-17 17:29:09 -0700

    updated infomodal

--- src/components/ImageContainer/ImageContainer.jsx
+++ src/components/ImageContainer/ImageContainer.jsx
@@ -2,26 +2,31 @@ import { Image, Flex, Text } from "@chakra-ui/react";
 
 export default function ImageContainer({ image, isresolved }) {
   return (
-    <Flex>
-      {isresolved && <Flex
-        backgroundColor={"rgba(255, 123, 0, 0.9)"}
-        position={"absolute"}
-        w={282}
-        justifyContent={"center"}
-        alignItems={"center"}
-        marginTop={90}
-        flexDir={"column"}
-      >
-        <Text fontSize={18} as="b" color={"white"} >RETURNED</Text>
-        <Text fontSize={15} color={"white"} >This item has been returned.</Text>
-      </Flex>}
-      <Image
-        rounded={"lg"}
-        height={230}
-        width={282}
-        objectFit={"cover"}
-        src={image}
-      />
+    <Flex
+      w={{ base: "100%", md: 450 }}
+      h={{ base: 400, md: 450 }}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      {isresolved && (
+        <Flex
+          backgroundColor={"rgba(255, 123, 0, 0.9)"}
+          position={"absolute"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          marginTop={30}
+          flexDir={"column"}
+          w={400}
+        >
+          <Text fontSize={18} as="b" color={"white"}>
+            RETURNED
+          </Text>
+          <Text fontSize={15} color={"white"}>
+            This item has been returned.
+          </Text>
+        </Flex>
+      )}
+      <Image w="100%" h="100%" rounded={"lg"} objectFit={"cover"} src={image} />
     </Flex>
   );
 }
